dc88d6737056775e9edd4afae2c610c2f1d14249
Orland <orlland.me@gmail.com>
2020-01-19 20:39:55 +1100

    Updates

---
+++ _next/static/webpack/static/development/pages/index.js.54c59de2ea93f55c0e0e.hot-update.js
@@ -0,0 +1,31 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./src/pages/index.jsx":
+/*!*****************************!*\
+  !*** ./src/pages/index.jsx ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return HomePage; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/orland/orlland.me/src/pages/index.jsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+function HomePage() {
+  return __jsx("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "orlland.me");
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.54c59de2ea93f55c0e0e.hot-update.js.map
